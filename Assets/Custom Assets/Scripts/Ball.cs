b7c4c214c016c2553afe017f7e03877310269c5a
Shane Do <nguyenshane@gmail.com>
2014-06-25 17:57:03 -0700

    Initial push

---
+++ Assets/Custom Assets/Scripts/Ball.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Ball : MonoBehaviour {
+
+	public Vector3 initialImpulse;
+
+	// Use this for initialization
+	void Start () {
+		rigidbody.AddForce(initialImpulse, ForceMode.Impulse);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision Collection)
+	{
+		if(Collection.gameObject.name == "Brick")
+		{
+			Destroy(Collection.gameObject);
+		}
+	}
+}
