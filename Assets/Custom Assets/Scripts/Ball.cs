d659e44a78e373e838f4439bb1309e60e28c1c31
mimlowe <michaellowe33@eduroam-227-87.ucsc.edu>
2014-06-26 13:37:22 -0700

    Override #3
    Overriding

--- Assets/Custom Assets/Scripts/Ball.cs
+++ Assets/Custom Assets/Scripts/Ball.cs
@@ -10,7 +10,10 @@ public enum eBall
 public class Ball : MonoBehaviour {
 
 	public Vector3 initialImpulse;
+
 	public eBall ball;
+	Vector3 leftImpulse = new Vector3(-2,0,0);
+	Vector3 rightImpulse = new Vector3(2,0,0);
 
 	// Use this for initialization
 	void Start () {
@@ -22,21 +25,34 @@ public class Ball : MonoBehaviour {
 	
 	}
 
+
+
 	void OnCollisionEnter(Collision Collection)
 	{
 		if (ball == eBall.Left) {
 			if(Collection.gameObject.name == "Brick")
 			{
 				Destroy(Collection.gameObject);
+				rigidbody.AddForce(leftImpulse, ForceMode.Impulse);
+
+
 			} else if (Collection.gameObject.name == "Brick_O") {
 				Destroy(Collection.gameObject);
+			}
+			else if (Collection.gameObject.name == "Player Left") {
+				rigidbody.AddForce(rightImpulse, ForceMode.Impulse);
 			}
 		} else if (ball == eBall.Right) {
 			if(Collection.gameObject.name == "Brick")
 			{
 				Destroy(Collection.gameObject);
+				rigidbody.AddForce(rightImpulse, ForceMode.Impulse);
+
 			} else if (Collection.gameObject.name == "Brick_G") {
 				Destroy(Collection.gameObject);
+			}
+			else if (Collection.gameObject.name == "Player Right") {
+				rigidbody.AddForce(leftImpulse, ForceMode.Impulse);
 			}
 		}
 	}
