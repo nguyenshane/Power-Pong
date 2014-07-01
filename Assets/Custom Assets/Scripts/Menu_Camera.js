ea5b03a1af6515f54c3d45368dcd96ebb2610325
Shane Do <nguyenshane@gmail.com>
2014-07-01 16:01:42 -0700

    changes to menu

---
+++ Assets/Custom Assets/Scripts/Menu_Camera.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+	var target = 270.0;
+	var speed = 45.0;
+	var angle : float = Mathf.MoveTowardsAngle
+			(transform.eulerAngles.y, target, speed * Time.deltaTime);
+		transform.eulerAngles = Vector3(angle, 0, 0);
+
+}
+
+function ToOptions(){
+
+}
