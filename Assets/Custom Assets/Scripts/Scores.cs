4fecf050971f76dc630c6965f98f6363b10ddaa1
Jesse Smith <jessesm24@live.com>
2014-07-05 21:37:38 -0700

    Added crappy score screen

--- Assets/Custom Assets/Scripts/Scores.cs
+++ Assets/Custom Assets/Scripts/Scores.cs
@@ -1,17 +1,26 @@
 using UnityEngine;
 using System.Collections;
 
+public enum eScore {
+	Green,
+	Orange
+}
+
 public class Scores : MonoBehaviour {
+
+	public eScore Score;
 
 	public GameObject scoreNumber;
 	public GameObject livesNumber;
-	public string nextLevel;
 
 	public float goalSpeedMultiplier;
 	public int maxLives;
+
 	private float currentMultiplier;
 	private int score;
 	private int lives;
+
+	private ScoreScreen scoreScreen;
 
 
 	// Use this for initialization
@@ -19,6 +28,7 @@ public class Scores : MonoBehaviour {
 		score = 0;
 		lives = maxLives;
 		currentMultiplier = 1;
+		scoreScreen = GameObject.Find ("ScoreScreen").GetComponent<ScoreScreen> ();
 	}
 	
 	// Update is called once per frame
@@ -29,19 +39,31 @@ public class Scores : MonoBehaviour {
 	public void SetScore (int newScore) {
 		score = newScore;
 		scoreNumber.guiText.text = score.ToString();
+
+		if (Score == eScore.Green) scoreScreen.greenScore = score;
+		else scoreScreen.orangeScore = score;
 	}
 
 	public void AddScore (int newScore) {
 		score += newScore;
 		scoreNumber.guiText.text = score.ToString();
+
+		if (Score == eScore.Green) scoreScreen.greenScore = score;
+		else scoreScreen.orangeScore = score;
 	}
 
 	public void RemoveLife() {
 		lives--;
 		livesNumber.guiText.text = lives.ToString();
-		
+
+		if (Score == eScore.Green) scoreScreen.greenLives = lives;
+		else scoreScreen.orangeLives = lives;
+
 		if (lives <= 0) {
-			Application.LoadLevel(nextLevel);
+			if (Score == eScore.Green) scoreScreen.orangeWins++;
+			else scoreScreen.greenWins++;
+
+			scoreScreen.activate();
 		}
 	}
 
